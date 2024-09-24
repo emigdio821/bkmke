a86d45e672e8b726bb3e2f96637c003760ff155a
Emigdio Torres <emigdio821@gmail.com>
2024-09-23 19:29:45 -0600

    feat: export bookmarks

--- components/settings/export-bookmarks.tsx
+++ components/settings/export-bookmarks.tsx
@@ -1,44 +1,54 @@
 'use client'
 
-import { IconFileDownload } from '@tabler/icons-react'
+import NiceModal from '@ebay/nice-modal-react'
+import { toast } from 'sonner'
+import { createClient } from '@/lib/supabase/client'
 import { Button } from '@/components/ui/button'
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+import { AlertActionDialog } from '@/components/dialogs/alert-action'
 
 export function ExportBookmarks() {
+  const supabase = createClient()
+
+  async function handleExportBookmarks() {
+    const { data, error } = await supabase.from('bookmarks').select('url')
+
+    if (error) {
+      console.error(error.message)
+      toast.error('Error', { description: 'Unable to export bookmarks at this time, try again.' })
+      return
+    }
+
+    const fileData = data.map((bk) => bk.url).join('\n')
+    const blob = new Blob([fileData], { type: 'text/plain' })
+    const url = URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.download = 'bkmk-bookmarks.txt'
+    link.href = url
+    link.click()
+  }
+
   return (
     <Card>
       <CardHeader className="justify-between gap-2 sm:flex-row">
         <div className="space-y-1.5">
           <CardTitle>Export bookmarks</CardTitle>
-          <CardDescription>Export your bookmarks as HTML or CSV file.</CardDescription>
+          <CardDescription>Export your bookmarks as a plain text file.</CardDescription>
         </div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button type="button" variant="outline">
-              Export
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Export as</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem disabled>
-              <IconFileDownload className="mr-2 size-4" />
-              <span className="font-mono">.html</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem disabled>
-              <IconFileDownload className="mr-2 size-4" />
-              <span className="font-mono">.csv</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Button
+          type="button"
+          variant="outline"
+          onClick={() => {
+            void NiceModal.show(AlertActionDialog, {
+              action: async () => {
+                await handleExportBookmarks()
+              },
+              message: 'This action will export all your bookmarks.',
+            })
+          }}
+        >
+          Export
+        </Button>
       </CardHeader>
     </Card>
   )
