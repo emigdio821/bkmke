288b4df2973d50226f3afbeca508fca38d21ec7f
Emigdio Torres <emigdio821@gmail.com>
2024-09-25 20:32:10 -0600

    revert: custom login og

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from 'next'
-import { appOgUrl, siteConfig } from '@/config/site'
+import { siteConfig } from '@/config/site'
 import { Footer } from '@/components/footer'
 
 export const metadata: Metadata = {
   title: {
     default: 'Log in',
     template: `%s · ${siteConfig.name}`,
-  },
-  openGraph: {
-    title: `Log in · ${siteConfig.name}`,
-    images: `${appOgUrl}?title=Log in`,
   },
 }
 
