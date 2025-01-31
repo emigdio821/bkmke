0e45d02b147ff477f40ffa845e108cce94f4f127
Emigdio Torres <emigdio821@gmail.com>
2025-01-30 18:59:36 -0600

    feat: next.js 15

--- app/(main)/tags/[id]/page.tsx
+++ app/(main)/tags/[id]/page.tsx
@@ -2,11 +2,12 @@ import { createClient } from '@/lib/supabase/server'
 import { TagitemsClientPage } from './page.client'
 
 interface TagItemsProps {
-  params: { id: string }
+  params: Promise<{ id: string }>
 }
 
-export async function generateMetadata({ params }: TagItemsProps) {
-  const supabase = createClient()
+export async function generateMetadata(props: TagItemsProps) {
+  const params = await props.params
+  const supabase = await createClient()
   const { data } = await supabase.from('tags').select().eq('id', params.id).order('name')
   const title = data?.[0]?.name || 'Tag items'
 
@@ -15,6 +16,7 @@ export async function generateMetadata({ params }: TagItemsProps) {
   }
 }
 
-export default function TagItemsPage({ params }: TagItemsProps) {
+export default async function TagItemsPage(props: TagItemsProps) {
+  const params = await props.params
   return <TagitemsClientPage id={params.id} />
 }
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,8 +3,9 @@ import { createServerClient } from '@supabase/ssr'
 import type { Database } from '@/types/database.types'
 import { envClientSchema } from '@/lib/schemas/client-env'
 
-export function createClient() {
-  const cookieStore = cookies()
+export async function createClient() {
+  const cookieStore = await cookies()
+
   return createServerClient<Database>(
     envClientSchema.NEXT_PUBLIC_SUPABASE_URL,
     envClientSchema.NEXT_PUBLIC_SUPABASE_ANON_KEY,
