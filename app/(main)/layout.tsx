0270dc4379e4d3c6e07260955b9f072e37f15dad
Emigdio Torres <emigdio821@gmail.com>
2025-06-26 14:10:25 -0600

    feat: added mobile header

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,7 @@
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
 
 interface MainLayoutProps {
@@ -10,6 +13,13 @@ export default function MainLayout({ children }: MainLayoutProps) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
+        <header className="flex h-12 items-center gap-2 border-b px-4 lg:hidden">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="data-[orientation=vertical]:h-4" />
+          <Button variant="ghost" className="h-7 px-2" asChild>
+            <Link href="/">bkmke</Link>
+          </Button>
+        </header>
         <div className="flex w-full flex-1 flex-col gap-4 p-4 xl:mx-auto xl:max-w-5xl">{children}</div>
       </SidebarInset>
     </SidebarProvider>
