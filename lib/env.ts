6db27c9cc49ff993bd05269a3c8e06a98c73cfde
Emigdio Torres <emigdio821@gmail.com>
2025-11-06 21:19:02 -0600

    fix: slow nextjs proxy due supabase
    temporary fix for this issue https://github.com/orgs/supabase/discussions/20905

--- lib/env.ts
+++ lib/env.ts
@@ -3,6 +3,7 @@ import { object, string, url } from 'zod'
 const envSchema = object({
   SUPABASE_URL: url().min(1, 'This env variable is required'),
   SUPABASE_ANON_KEY: string().min(1, 'This env variable is required'),
+  SUPABASE_SECRET: string().min(1, 'This env variable is required'),
 })
 
 export const serverEnv = envSchema.parse(process.env)
--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,28 @@
 import type { NextRequest } from 'next/server'
-import { updateSession } from '@/lib/supabase/middleware'
+import { NextResponse } from 'next/server'
+// import { updateSession } from '@/lib/supabase/middleware'
+import { serverEnv } from './lib/env'
+import { SupabaseSafeSession } from './lib/supabase-safe-session'
+import { createClient } from './lib/supabase/server'
 
 export async function proxy(req: NextRequest) {
-  return await updateSession(req)
+  // return await updateSession(req)
+
+  const supabase = await createClient()
+  const safeSession = new SupabaseSafeSession(supabase, serverEnv.SUPABASE_SECRET)
+  const { data, error } = await safeSession.getUser()
+
+  if (req.nextUrl.pathname === '/login' && data) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  if (req.nextUrl.pathname !== '/login' && (error || !data)) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  NextResponse.next({
+    request: req,
+  })
 }
 
 export const config = {
